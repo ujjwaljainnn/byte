df60122b2b1cb257e46dad762c8007e3a8978f08
thesmashten <70218698+thesmashten@users.noreply.github.com>
2023-02-09 16:50:22 -0600

    form page in progress

--- app/routes/signup/index.tsx
+++ app/routes/signup/index.tsx
@@ -1,6 +1,6 @@
 // file: app/routes/index.js
 
-import { Box, Container, Grid } from "@mui/material";
+import { Box, Container, Grid, TextField } from "@mui/material";
 import React, { Component } from 'react';
 import { Form, useSearchParams } from "@remix-run/react";
 import { SocialsProvider } from "remix-auth-socials";
@@ -9,10 +9,16 @@ import { fontSize } from "@mui/system";
 
 const CONTAINER_STYLES = {
   width: "100%",
-  display: "flex",
+  display: "grid",
   justifyContent: "center",
   alignItems: "center",
 };
+
+const CONTAINER_STYLES_2 = {
+    justifyContent: "center",
+    alignItems: "left",
+    display: "grid",
+  };
 
 const BUTTON_STYLES = {
   padding: "15px 25px",
@@ -28,6 +34,7 @@ const mystyle = {
     fontFamily: "Roboto",
     fontWeight: "bold",
     fontSize: "30px",
+    display: "flex",
   };
 
 
@@ -37,6 +44,14 @@ export default function SignUp() {
   return (
     <Container style = {CONTAINER_STYLES}>     
         <h1 style={mystyle}>Sign Up</h1>
+        <Container style = {CONTAINER_STYLES_2}>
+            <TextField id="standard-basic" label="Username" variant="standard" />    
+            <TextField id="standard-basic" label="Email" variant="standard" />    
+            <TextField id="standard-basic" label="First Name" variant="standard" />    
+            <TextField id="standard-basic" label="Last Name" variant="standard" />    
+            <TextField id="standard-basic" label="Year" variant="standard" />    
+            <TextField id="standard-basic" label="Password" variant="standard" />    
+        </Container>
     </Container>
   );
 }
