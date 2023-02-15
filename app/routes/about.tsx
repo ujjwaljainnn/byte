8bec63292fd2a9e4a99ed19fa7127cc26952208c
Kunal Kaushik <kunal.kaushik@vanderbilt.edu>
2023-02-15 02:08:15 -0600

    added functionality flowchart

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,13 +1,13 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Grid from '@mui/material/Grid';
-import Item from '@mui/material/Grid';
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Grid from "@mui/material/Grid";
+import Item from "@mui/material/Grid";
 
-function CardRow(props : {name: string, description: string}) {
+function CardRow(props: { name: string; description: string }) {
   return (
     <Card sx={{ minWidth: 275, minHeight: 250 }}>
       <CardContent>
@@ -21,7 +21,7 @@ function CardRow(props : {name: string, description: string}) {
           }}
           src={require("../../public/images/download.jpg")}
         />
-        <Typography variant="h6" component="div" align="center" sx={{ my: 1}}>
+        <Typography variant="h6" component="div" align="center" sx={{ my: 1 }}>
           {props.name}
         </Typography>
         <Typography sx={{ fontSize: 14 }} color="text.secondary" align="center">
@@ -34,13 +34,15 @@ function CardRow(props : {name: string, description: string}) {
 
 export default function AboutPage() {
   return (
-    <Grid style={{
-      minWidth: "100%",
-      height: "100vh",
-    }}>
-      <Grid container spacing={2} sx={{ my: 3 }} ml = {2}>
-        <Grid item xs={8} md={6} >
-          <Box sx={{ my: 5}}>
+    <Grid
+      style={{
+        minWidth: "100%",
+        height: "100vh",
+      }}
+    >
+      <Grid container spacing={2} sx={{ my: 3 }} ml={2}>
+        <Grid item xs={8} md={6}>
+          <Box sx={{ my: 5 }}>
             <Typography variant="h3" gutterBottom align="left">
               About Byte
             </Typography>
@@ -48,13 +50,16 @@ export default function AboutPage() {
               Bringing people together through food
             </Typography>
             <Typography variant="subtitle1" gutterBottom align="left">
-              Recognizing that there are tons of great restaurants in Nashville but not enough people have an incentive to try them, 
-              we at Byte aim to bring people together through food, allowing Vanderbilt students to meet new people who enjoy the 
-              same restaurants by setting them up on a meal meetup. 
+              Recognizing that there are tons of great restaurants in Nashville
+              but not enough people have an incentive to try them, we at Byte
+              aim to bring people together through food, allowing Vanderbilt
+              students to meet new people who enjoy the same restaurants by
+              setting them up on a meal meetup.
             </Typography>
             <Typography variant="subtitle1" gutterBottom align="left">
-              Begin by registering for an account using your Vanderbilt email, and selecting a few restaurants you hope to try. 
-              Byte will do the rest and you will be eating in no time.
+              Begin by registering for an account using your Vanderbilt email,
+              and selecting a few restaurants you hope to try. Byte will do the
+              rest and you will be eating in no time.
             </Typography>
           </Box>
         </Grid>
@@ -65,32 +70,52 @@ export default function AboutPage() {
           />
         </Grid>
       </Grid>
+      <Grid container spacing={2} sx={{ my: 6 }} ml={6}>
+        <Grid>
+          <Box
+            component="img"
+            src={require("../../public/images/flowchart.png")}
+          />
+        </Grid>
+      </Grid>
       <Grid mt={15}>
         <Box sx={{ my: 3 }}>
           <Typography variant="h4" gutterBottom align="center">
             Meet the team
           </Typography>
         </Box>
-        <Box sx={{ my: 4 }} >
+        <Box sx={{ my: 4 }}>
           <Grid container spacing={2} sx={{ my: 3 }}>
-            <Grid  item xs={3}>
+            <Grid item xs={3}>
               <Item>
-                <CardRow name={"Kunal Kaushik"} description={"kunal description"}/>
+                <CardRow
+                  name={"Kunal Kaushik"}
+                  description={"kunal description"}
+                />
               </Item>
             </Grid>
-            <Grid  item xs={3}>
+            <Grid item xs={3}>
               <Item>
-                <CardRow name={"Lucas Smulders"} description={"lucas description"}/>
+                <CardRow
+                  name={"Lucas Smulders"}
+                  description={"lucas description"}
+                />
               </Item>
             </Grid>
-            <Grid  item xs={3}>
+            <Grid item xs={3}>
               <Item>
-                <CardRow name={"Ujjwal Jain"} description={"ujjwal description"}/>
+                <CardRow
+                  name={"Ujjwal Jain"}
+                  description={"ujjwal description"}
+                />
               </Item>
             </Grid>
-            <Grid  item xs={3}>
+            <Grid item xs={3}>
               <Item>
-                <CardRow name={"Nishant Jain"} description={"nishant description"}/>
+                <CardRow
+                  name={"Nishant Jain"}
+                  description={"nishant description"}
+                />
               </Item>
             </Grid>
           </Grid>
