7f57ea507bcebc2908ee5817e5f5751598555bb0
Ujjwal Jain <ujjwal.jain@vanderbilt.edu>
2023-02-27 16:48:00 -0600

    no upcoming meetups case handled

--- app/routes/dashboard/index.tsx
+++ app/routes/dashboard/index.tsx
@@ -26,21 +26,40 @@ export const loader = async ({ request }: any) => {
 
   const meetups = await getUserMeetups(userId);
 
-  if (!meetups) {
+  console.log("meetups", meetups);
+
+  if (meetups?.length === 0 || !meetups) {
+    console.log("no meetups");
     return {
       user,
       meetups,
     };
   }
+
+  const match = await getMeetupMatch(userId, meetups ? meetups[0].id : "0");
+
+  if (!match) {
+    return {
+      user,
+      meetups: [],
+    };
+  }
+
+  const restaurant_name_meetup = await getRestaurant(
+    meetups ? meetups[0].restaurantId : "0"
+  );
+
   return {
     user,
     meetups,
+    match,
+    restaurant_name_meetup,
   };
 };
 
 const Dashboard = () => {
   // getting user from loader data
-  const { user, meetups } = useLoaderData();
+  const { user, meetups, match, restaurant_name_meetup } = useLoaderData();
 
   // make a timer that shows time to upcoming Sunday 6:00 PM in hours and minutes
   const now = new Date();
@@ -97,6 +116,41 @@ const Dashboard = () => {
             {timeToNextSunday % 60} seconds
           </Typography>
         </Paper>
+        {meetups.length > 0 ? (
+          <Paper
+            sx={{
+              marginTop: 2,
+              padding: 2,
+              boxShadow: 3,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Typography component="h1" variant="h5">
+              Your next match is:{" "}
+              <Link href={`/profile/${match[0].id}`}>
+                {match[0].first_name + " " + match[0].last_name}
+              </Link>{" "}
+              at {restaurant_name_meetup.name}
+            </Typography>
+          </Paper>
+        ) : (
+          <Paper
+            sx={{
+              marginTop: 2,
+              padding: 2,
+              boxShadow: 3,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Typography component="h1" variant="h5">
+              You have no upcoming meetups
+            </Typography>
+          </Paper>
+        )}
       </Box>
     </Container>
   );
